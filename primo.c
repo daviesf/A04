f7b7496da0876f2f4566b8e6c69e89c7006446bc
Davie <davie.schimidtfonseca@gmail.com>
2024-08-26 17:54:54 -0300

    Update primo.c

--- primo.c
+++ primo.c
@@ -41,26 +41,3 @@ int verificaNumero(float n)
     else
         return 0;
 }
-
-// Bloco principal.
-int main()
-{
-    float num;
-    printf("Digite um numero: ");
-    scanf("%f", &num);
-
-    if (verificaNumero(num))
-    {
-        printf("Numero invalido\n");
-        return 1;
-    }
-
-    int n = (int) num;
-
-    if (primo(n))
-        printf("O numero %d e primo\n", n);
-    else
-        printf("O numero %d nao e primo. O proximo primo e %d\n", n, proxPrimo(n));
-
-    return 0;
-}
